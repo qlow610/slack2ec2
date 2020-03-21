e175bbde8e9300419b49068eb562daf199b15974
qlow610 <soma@somanoMacBook-Pro.local>
2020-03-21 22:22:36 +0900

    commit history fix

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,5 +1,4 @@
-# coding:utf-8
-
+"""This is Slack to Ec2/NSG operating """
 #############################
 #language pytho
 #ec2 start/stop/status/SG Change
@@ -8,241 +7,120 @@
 from __future__ import print_function
 
 #import
+from pprint import pprint
 import boto3
-import json 
-import pprint
-import re
-import logging
-import os
-import zlib
-import urllib.request
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
+import json
 
 #
-ec2 = boto3.client('ec2')
-ec3 = boto3.resource('ec2')
+EC2 = boto3.client('ec2')
+
 
 def dict_create():
-    global instance_dict
-    instance_dict = {}
-    list1 = ec2.describe_instances(
-        Filters = [{'Name':'tag:Slack','Values':['']}]
-    )
-    for IPRange in list1['Reservations']:
-        for list3 in IPRange['Instances']:
-            instanceid = list3['InstanceId']
-            NSG = list3['NetworkInterfaces'][0]['Groups'][0]['GroupId']
-            Status = list3["State"]['Name']
-            instance = ec3.Instance(id=instanceid)
-            name_tag = [x['Value'] for x in instance.tags if x['Key'] == 'Name']
-            name = name_tag[0] if len(name_tag) else ''
-            Dict_Key = ['instanceid','Name','Status','NSG']
-            Dict_value = [instanceid,name,Status,NSG]
-            instance_dict[name] = dict(zip(Dict_Key, Dict_value))
-    return(instance_dict)
+    """
+    Instance Dict Create
+    """
+    ins_dict = {}
+    temp_list1 = EC2.describe_instances()
+    for iprange in temp_list1['Reservations']:
+        for temp_list2 in iprange['Instances']:
+            instanceid = temp_list2['InstanceId']
+            nsg = temp_list2['NetworkInterfaces'][0]['Groups'][0]['GroupId']
+            status = temp_list2["State"]['Name']
+            name_tag = [x["Value"] for x in temp_list2['Tags'] if x['Key'] == 'Name']
+            name = name_tag[0] if name_tag else ''
+            dict_key = ['instanceid', 'Name', 'Status', 'NSG']
+            dict_value = [instanceid, name, status, nsg]
+            ins_dict[name] = dict(zip(dict_key, dict_value))
+    return ins_dict
 
-def Instance_Status(instance_dict):
-    global message
-    Instance_list = []
+def instance_status(instance_dict):
+    """
+    Instance Status Notification
+    """
+    temp_list = []
     for j in instance_dict.keys():
-        Instance_list.append(j + ":" + instance_dict[j]['Status'] )
-    message = ""
-    message = '\n'.join(Instance_list)
-    return(message)
+        temp_list.append(j + ":" + instance_dict[j]['Status'])
+    message = '\n'.join(temp_list)
+    return message
 
-def Instance_Action(Action,body,instance_dict):
-    global message
-    for j in instance_dict.keys():
-        if instance_dict[j]['Name'] in body:
-            Target = instance_dict[j]['Name']
-            Targetid = instance_dict[Target]['instanceid']
-        else :
-            message = "No Target.You're probably misspelled."
-            success = 1
-        if 'start' in Action:
-            print('start')
-            try :
-                response = ec2.start_instances(InstanceIds=[Targetid])
-                Status = response['StartingInstances'][0]['CurrentState']['Name']
-                success = 0
-            except:
-                message = "No Target.You're probably misspelled."            
-        elif 'stop' in Action:
-            print('stop')
-            try:
-                response = ec2.stop_instances(InstanceIds=[Targetid])
-                pprint.pprint(response)
-                Status = response['StoppingInstances'][0]['CurrentState']['Name']
-                success = 0
-            except:
-                message = "No Target.You're probably misspelled."            
+def instance_action(action, body, instance_dict):
+    """
+    Operation Instance Start/Stop
+    action: start / stop
+    body : instanceid
+    instance_dict : func dict_create
+    """
+    try:
+        target_name = [x for x in instance_dict.keys() if x in body]
+        target_id = instance_dict[target_name[0]]['instanceid']
+    except IndexError:
+        message = "No Target.You're probably misspelled."
+        return message
+    try:
+        if 'start' in action:
+            print("action start")
+            response = EC2.start_instances(InstanceIds=[target_id])
+            current_status = response['StartingInstances'][0]['CurrentState']['Name']
+        elif 'stop' in action:
+            print("action stop")
+            response = EC2.stop_instances(InstanceIds=[target_id])
+            current_status = response['StoppingInstances'][0]['CurrentState']['Name']
         else:
             message = "No Target.You're probably misspelled."
-            success = 1
-        if success == 0:
-            message = "You " + Action + " " + Target + ". The current status is " + Status  + "."
-    return(message)
+            return message
+        message = "You " + action + " " + target_name[0] \
+            + ". The current status is " + current_status  + "."
+    except IndexError:
+        message = "No Target.You're probably misspelled."
+    return message
 
 def whoname(body):
-    global user_id
-    Body_list = [j for j in body.split('&') if 'user_id' in j]
-    user_id = str(Body_list)
-    user_id = user_id.replace('user_id=','')
+    """
+    slack userid
+    """
+    body_list = [j for j in body.split('&') if 'user_id' in j]
+    user_id = str(body_list)
+    user_id = user_id.replace('user_id=', '')
     del_table = str.maketrans('', '', "[]'")
     user_id = user_id.translate(del_table)
     user_id = "<@" + user_id + ">"
-    return(user_id)
+    return user_id
 
-def NGmessage():
-    global message
-    message = "$server (status | help | [server name] start | [server name] stop | ipshow | ipadd/ipdell [server name] -FromPort xx -IpRanges xx.xx.xx.xx/xx -Description xxxx"
-    return(message)
+def default_message():
+    """
+    NG/help message
+    """
+    message = "$server (status | help | [server name] start \
+        | [server name] stop | ipshow \
+        | ipadd/ipdell [server name] -FromPort xx -IpRanges xx.xx.xx.xx/xx -Description xxxx"
+    return message
 
-def NSG_list(instance_dict):
-    global message
-    message = []
-    for j in instance_dict.keys():
-        Tergetid = instance_dict[j]['NSG']
-        Describe_SG = ec2.describe_security_groups(GroupIds=[Tergetid])
-        IpPermission = Describe_SG['SecurityGroups'][0]['IpPermissions']
-        Count = 0
-        for Iptable in IpPermission:
-            IPRange = Iptable['IpRanges']
-            Port_list = Iptable['FromPort']
-            for k in IPRange:
-                if Count is 0:
-                    message.append('[ ' + j + ' ]' + '\n'+"SGID: "+  Tergetid)
-                    Count = 1
-                if 'Description' in k.keys():
-                    message.append("Port :" + str(Port_list) + '\n' + k['CidrIp'] + " \n  Description :" +k['Description'] )  
-                else:
-                    message.append( k['CidrIp'] + "\n  Description :") 
-    message = '\n'.join(message)
-    print(message)
-    return(message)
-
-def Bodysplit(Action,body):
-    global FPort
-    global IpRange
-    global Description
-    Body_list = [j for j in body.split('&') if 'text' in j]
-    Body_list = str(Body_list)
-    del_table = str.maketrans('', '', "[]'")
-    Body_list = Body_list.translate(del_table)
-    Body_list = [j for j in str(Body_list).split('+')]
-    print(Body_list)
-    FromPortNum = (Body_list.index("-FromPort") + 1)
-    FPort = int(Body_list[FromPortNum])
-    IpPangesNum = (Body_list.index("-IpRanges") + 1)
-    IpRange = (Body_list[IpPangesNum]).replace('%2F','/')
-    print(IpRange)
-    if 'ipadd' in  Action:
-        DescriptionNum = (Body_list.index("-Description") + 1)
-        Description = Body_list[DescriptionNum]
-    else:
-        Description = ""
-    return(FPort,IpRange,Description)
-
-
-def NSG_add(body,instance_dict,FPort,IpRange,Description):
-    global message
-    for j in instance_dict.keys():
-        if instance_dict[j]['Name'] in body:
-            Target = instance_dict[j]['Name']
-            print(Target)
-            Tergetid = instance_dict[j]['NSG']
-            print(Tergetid)
-            security_group = ec3.SecurityGroup(Tergetid)
-        else:
-            message = "No Target.You're probably misspelled."
-    try:
-        response = security_group.authorize_ingress(
-            DryRun=False,
-            IpPermissions=[
-                {
-                    'FromPort': FPort,
-                    'IpProtocol': 'tcp',
-                    'IpRanges': [
-                        {
-                            'CidrIp': IpRange,
-                            'Description': Description
-                            },
-                            ],
-                            'ToPort': FPort,
-                            }])
-        logger.info(response)
-        message = "NSG add Success"
-    except:
-        message = "NSG add Failed"
-    return(message)
-
-def NSG_dell(body,instance_dict,FPort,IpRange):
-    global message
-    for j in instance_dict.keys():
-        if instance_dict[j]['Name'] in body:
-            Target = instance_dict[j]['Name']
-            print(Target)
-            Tergetid = instance_dict[j]['NSG']
-            print(Tergetid)
-            security_group = ec3.SecurityGroup(Tergetid)
-        else:
-            message = "No Target.You're probably misspelled."
-    try:
-        response = security_group.revoke_ingress(
-            DryRun=False,
-            IpPermissions=[
-                {
-                    'FromPort': FPort,
-                    'IpProtocol': 'tcp',
-                    'IpRanges': [
-                        {
-                            'CidrIp': IpRange
-                            },
-                            ],
-                            'ToPort': FPort
-                            }])
-        logger.info(response)
-        message = "NSG dell Success"
-    except:
-        message = "NSG dell Failed"
-    return(message)
-
-#メイン処理
-
-def lambda_handler(event, context):
-    dict_create()
+def lambda_handler(event,context):
+    """
+    main
+    """
+    instance_dict = dict_create()
     body = str(event['body'])
-    logger.info(event)
-    print(body)
-    logger.info(instance_dict)
     if 'status' in body:
-        logger.info(Instance_Status(instance_dict))
+        message = instance_status(instance_dict)
     elif 'start' in body:
-        Action = 'start'
-        logger.info(Instance_Action(Action,body,instance_dict))
+        action = 'start'
+        message = instance_action(action, body, instance_dict)
     elif 'stop' in body:
-        Action = 'stop'
-        logger.info(Instance_Action(Action,body,instance_dict))
-    elif 'ipshow' in body:
-        logger.info(NSG_list(instance_dict))
-    elif "ipadd" in body:
-        print("ipadd")
-        Action = "ipadd"
-        logger.info(Bodysplit(Action,body))
-        logger.info(NSG_add(body,instance_dict,FPort,IpRange,Description))
-    elif "ipdell" in body:
-        Action = 'ipdell'
-        logger.info(Bodysplit(Action,body))
-        logger.info(NSG_dell(body,instance_dict,FPort,IpRange))
+        action = 'stop'
+        message = instance_action(action, body, instance_dict)
     else:
-        NGmessage()
-    whoname(body)
-    message_json = user_id + '\n' + message
-    message_json = json.dumps({'text': message_json})
-    pprint.pprint(message_json)
+        message = default_message()
+    user_id = whoname(body)
+    if "ipshow" in body:
+        message_json = json.dumps(
+            {
+                'text': user_id,
+                'attachments': message}
+            )
+    else:
+        message_json = user_id + '\n' + message    
+        message_json = json.dumps({'text': message_json})
     return {
         'statusCode': 200,
         'body': message_json
